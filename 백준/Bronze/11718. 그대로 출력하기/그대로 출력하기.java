604e5f4c632adf3c3fb0fbfaa1985d6989cb0c50
Daehyun-Kim <kdh031230@gmail.com>
2026-01-23 01:54:15 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            System.out.println(input);
+        }
+    }
+}

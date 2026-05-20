1c4daace737bd2270da65d268f51e1d7b95bbda7
Daehyun Kim <kdh031230@gmail.com>
2026-05-20 10:30:06 +0900

    [level 3] Title: 단속카메라, Time: 4.06 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int camera = routes[0][1];
+        int answer = 1;
+        
+        for (int i = 1; i < routes.length; i++) {
+            if (routes[i][0] > camera) {
+                camera = routes[i][1];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

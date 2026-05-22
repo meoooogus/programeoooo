6e2aff7e54f6416ed4fd0af3b6e4481847c2fc5a
Daehyun Kim <kdh031230@gmail.com>
2026-05-22 13:24:25 +0900

    [level 2] Title: 소수 찾기, Time: 15.76 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    Set<Integer> primes = new HashSet<>();
+    
+    public int solution(String numbers) {
+        char[] chs = numbers.toCharArray();
+        visited = new boolean[numbers.length()];
+        dfs(chs, "");
+        
+        return primes.size();
+    }
+    
+    public void dfs(char[] chs, String cur) {
+        if (!cur.equals("") && isPrime(Integer.parseInt(cur)))
+            primes.add(Integer.parseInt(cur));
+        
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i] == true)
+                continue;
+            visited[i] = true;
+            dfs(chs, cur + chs[i]);
+            visited[i] = false;
+        }
+    }    
+    public boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                return false;
+        
+        return true;
+    }
+}

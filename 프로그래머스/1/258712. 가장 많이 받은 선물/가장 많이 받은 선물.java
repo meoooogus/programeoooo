bf16113122c8fcc4f894ca5ed47347e2502f67ba
Daehyun Kim <kdh031230@gmail.com>
2026-07-02 12:45:17 +0900

    compare 로직 통합 및 단순화
    Refactor comparison logic to simplify the process of determining the most received gifts by removing redundant methods.

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -29,25 +29,13 @@ class Solution {
             for (int j = i; j < n; j++) {
                 if (i == j)
                     continue;
-                int cmpHis = compareHistory(i, j, history);
-                if (cmpHis == 1) receive[i]++;
-                else if (cmpHis == -1) receive[j]++;
-                else {
-                    int cmpMet = compareMetric(i, j, metric);
-                    if (cmpMet == 1) receive[i]++;
-                    else if (cmpMet == -1) receive[j]++;
-                }
+                int cmp = Integer.compare(history[i][j], history[j][i]);
+                if (cmp == 0) cmp = Integer.compare(metric[i], metric[j]);
+                if (cmp > 0) receive[i]++;
+                else if (cmp < 0) receive[j]++;
             }
         }
         
         return Arrays.stream(receive).max().getAsInt();
-    }
-    
-    private int compareHistory(int i, int j, int[][] history) {
-        return history[i][j] > history[j][i] ? 1 : history[i][j] < history[j][i] ? -1 : 0;
-    }
-    
-    private int compareMetric(int i, int j, int[] metric) {
-        return metric[i] > metric[j] ? 1 : metric[i] < metric[j] ? -1 : 0;
     }
 }

a7e4e3c0cc3cfcf84620803a90a45587f8b8707b
Daehyun Kim <kdh031230@gmail.com>
2026-07-09 12:38:53 +0900

    핵심 로직에 대한 주석 추가
    Add comments explaining the logic for sorting and targeting missiles.

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
 
+// 끝점을 기준으로 오름차순 정렬하면,
+// 가장 이른 끝점에 미사일을 쏘는 것이 항상 최적이다
+// 마지막에 쏜 미사일과 각각의 미사일과 비교하며 값 초기화
+
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;

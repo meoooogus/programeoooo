211ce5ff2428c3d6fea42c6d7f6ff642cfaf37d7
Daehyun Kim <meoooogus@gmail.com>
2026-08-19 10:25:34 +0900

    [Passed] 반복 출력하기 (134ms, 10MB)

---
+++ codetree/trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        print(sc.nextInt());
+    }
+    private static void print(int N) {
+        for (int i = 0; i < N; i++)
+            System.out.println("12345^&*()_");
+    }
+}

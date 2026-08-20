db64788b82b5dee8fd5919f528259a9bd89b9203
Daehyun Kim <meoooogus@gmail.com>
2026-08-20 10:58:29 +0900

    [Passed] 정수의 최솟값 (126ms, 10MB)

---
+++ codetree/trail2/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(min(a, b, c));
+    }
+    private static int min(int a, int b, int c) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        return min;
+    }
+}

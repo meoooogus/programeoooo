9bc3a51962a87c188ecf447e844994f53f3f81b9
Daehyun Kim <meoooogus@gmail.com>
2026-08-24 09:37:02 +0900

    [Passed] 함수를 이용한 윤년 판별 (126ms, 10MB)

---
+++ codetree/trail2/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(yn(y));
+    }
+    private static boolean yn(int n) {
+        if (n % 4 != 0)
+            return false;
+        if (n % 100 == 0 && n % 400 != 0)
+            return false;
+        return true;
+    }   
+}
